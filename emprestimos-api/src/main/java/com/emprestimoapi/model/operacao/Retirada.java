ecbb9f4cbe645acff3c2df62b104f493a5187683
fabiano <fabiano@fabiano-Inspiron-5458>
2019-07-13 15:02:12 -0300

    Ultimo

--- emprestimos-api/src/main/java/com/emprestimoapi/model/operacao/Retirada.java
+++ emprestimos-api/src/main/java/com/emprestimoapi/model/operacao/Retirada.java
@@ -4,7 +4,6 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import com.emprestimoapi.model.entidade.Usuario;
 
 @Entity
 @DiscriminatorValue("retirada")
@@ -14,23 +13,11 @@ public class Retirada extends Operacao{
 	@JoinColumn(name="id_motivo_operacao")
 	private TipoEntrada motivoOperacao;
 
-	@ManyToOne
-	@JoinColumn(name="id_beneficiario")
-	private Usuario beneficiario;
-
 	public TipoEntrada getMotivoOperacao() {
 		return motivoOperacao;
 	}
 
 	public void setMotivoOperacao(TipoEntrada motivoOperacao) {
 		this.motivoOperacao = motivoOperacao;
-	}
-
-	public Usuario getBeneficiario() {
-		return beneficiario;
-	}
-
-	public void setBeneficiario(Usuario beneficiario) {
-		this.beneficiario = beneficiario;
 	}
 }
